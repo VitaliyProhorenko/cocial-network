b567da3d82da10ea0126e867b19b38511a4289e2
Vitaliy Prohorenko <GoonzaPVS@yandex.by>
2021-08-28 15:18:21 +0300

    added social-network first files from lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="http://i.imgur.com/r9QO3.png" alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
