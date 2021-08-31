085a4744852903ef7e725d067c471d97ae8bbaaa
Vitaliy Prohorenko <GoonzaPVS@yandex.by>
2021-08-31 16:16:58 +0300

    added css-modules from lesson 14.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img src="http://i.imgur.com/r9QO3.png" alt="logo"/>
         </header>
     );
