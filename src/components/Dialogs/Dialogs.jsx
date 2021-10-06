4ec2d4b74cc0629838fa0c44cf9a5a592eb9a708
Vitaliy Prohorenko <GoonzaPVS@yandex.by>
2021-10-06 19:44:59 +0300

    added content to Dialogs page from lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/1">Dimych</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Sasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Victor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Valera</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className="message">Hi</div>
+                <div className="message">How is your it-kamasutra?</div>
+                <div className="message">Yo</div>
+            </div>
         </div>
     );
 }
