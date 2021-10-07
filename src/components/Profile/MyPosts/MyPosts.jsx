7e48d68a2533238a3a9c2b668636997e38c5f6bc
Vitaliy Prohorenko <GoonzaPVS@yandex.by>
2021-10-07 18:44:44 +0300

    added and refactored data array to Dialogs and MyPosts pages from lesson 24

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postsData = [
+        {id: 1, message: "Hi, how are you?", likesCount: 23},
+        {id: 2, message: "It's my first post", likesCount: 0},
+        {id: 3, message: "Hello world!", likesCount: 3},
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -15,9 +22,9 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message="Hi, how are you?" likesCount="23"/>
-                <Post message="It's my first post" likesCount="0"/>
-                <Post message="Hello world!" likesCount="3"/>
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+                <Post message={postsData[2].message} likesCount={postsData[2].likesCount}/>
             </div>
         </div>
     );
